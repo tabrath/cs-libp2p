23b1b4b8c13b82afc924a6d3dc1bb79185adfc5b
Trond Bråthen <tabrath@gmail.com>
2017-03-10 21:30:22 +0100

    Squashed 'LibP2P.Protocol/' content from commit 8e7b4d1
    git-subtree-dir: LibP2P.Protocol
    git-subtree-split: 8e7b4d142c27f90f27662eb7f2461ab1f125ffd4

---
+++ LibP2P.Protocol/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LibP2P.Protocol")]
+[assembly: AssemblyDescription("libp2p protocol")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tabrath")]
+[assembly: AssemblyProduct("LibP2P.Protocol")]
+[assembly: AssemblyCopyright("Copyright © tabrath 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce416346-7955-4118-8d71-44d9077bce36")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
---
+++ LibP2P.Protocol/ProtocolId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LibP2P.Protocol
+{
+    public class ProtocolId : IEquatable<ProtocolId>
+    {
+        public static readonly ProtocolId TestingId = new ProtocolId("/p2p/_testing");
+
+        private readonly string _value;
+
+        public ProtocolId(string value)
+        {
+            _value = value;
+        }
+
+        public override string ToString() => _value;
+        public override bool Equals(object obj) => obj is ProtocolId && Equals((ProtocolId) obj);
+        public bool Equals(ProtocolId other) => _value.Equals(other?._value);
+        public override int GetHashCode() => _value.GetHashCode();
+
+        public static implicit operator ProtocolId(string s) => new ProtocolId(s);
+        public static implicit operator string(ProtocolId id) => id._value;
+    }
+}
